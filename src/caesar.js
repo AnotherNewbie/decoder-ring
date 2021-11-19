c581d4416d95506ecfc9ca213768301e170d7503
Michael Holthausen <mgholthausen@gmail.com>
2021-11-19 15:04:48 -0500

    fixed issue

--- src/caesar.js
+++ src/caesar.js
@@ -11,35 +11,32 @@ const caesarModule = (function () {
     // input is code to be shifted
     // shift is ammount code is to be shifted or ammount to shift back
     // when encode ===false flip sign of shift
-    try {
-      if (shift > 25 || shift < -25 || shift === 0) {
-        throw "Invalid shift ammount";
+    try{      
+      if( shift > 25 || shift < -25 || shift === 0){
+        throw "Invalid shift ammount"
       }
-      if (encode === false) {
+      if (encode === false){
         shift = shift * -1;
-      }
+      }       
       let code = input.toLowerCase();
       let result = "";
-      for (let i = 0; i < code.length; i++) {
-        if (code.charCodeAt(i) >= 97 && code.charCodeAt(i) <= 122) {
-          // check that the ascii is within a-z
-          if (code.charCodeAt(i) + shift < 97) {
-            // check to see if the shift will make it less than a
-            shift = shift + 26; //  roll over shift change to make it start at z
-            result += String.fromCharCode(code.charCodeAt(i) + shift);
-          } else if (code.charCodeAt(i) + shift > 122) {
-            //check to see if shift is greater than z
-            shift = shift - 26; // roll over shift to start at a
-            result += String.fromCharCode(code.charCodeAt(i) + shift);
+      for(let i = 0; i < code.length; i++){
+          if(code.charCodeAt(i) >= 97 && code.charCodeAt(i) <= 122){  // check that the ascii is within a-z                      
+            if((code.charCodeAt(i)+ shift) < 97){                     // check to see if the shift will make it less than a
+               shift = shift + 26;                                    //  roll over shift change to make it start at z
+              result += (String.fromCharCode((code.charCodeAt(i) + shift)));    
+            }else if((code.charCodeAt(i)+ shift)  > 122){                 //check to see if shift is greater than z
+              shift = shift -26;                                          // roll over shift to start at a
+              result += (String.fromCharCode((code.charCodeAt(i) + shift)));   
+           }else {            
+            result += (String.fromCharCode((code.charCodeAt(i) + shift)));  
+           }
           } else {
-            result += String.fromCharCode(code.charCodeAt(i) + shift);
+          result += code.charAt(i);           //for ascii characters not within a to z just copy them
           }
-        } else {
-          result += code.charAt(i); //for ascii characters not within a to z just copy them
-        }
       }
       return result;
-    } catch (error) {
+    }catch (error){
       return false;
     }
   }
