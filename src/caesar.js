b94d3031774f2145a3691e41cd6d59d25f4308f8
Michael Holthausen <mgholthausen@gmail.com>
2021-11-19 11:51:36 -0500

    worked on project some

--- src/caesar.js
+++ src/caesar.js
@@ -8,6 +8,31 @@ const caesarModule = (function () {
 
   function caesar(input, shift, encode = true) {
     // your solution code here
+    // input is code to be shifted
+    // shift is ammount code is to be shifted or ammount to shift back
+    // when encode ===false flip sign of shift
+    try{
+      if (encode === false){
+        shift = shift * -1;
+      }
+      if( shift > 25 || shift < 26 || shift === 0){
+        throw "Invalid shift ammount"
+      }        
+      const result = input.toLowerCase();
+      for(let i = 0; i < result.length; i++){
+        if(result >= 97 && result <= 122){
+          if((result.charAt(i)+ shift) >=97 || (result.charCodeAt(i)+ shift )<= 122){
+
+          }else {
+            result.charCodeAt(i) = result.charCodeAt(i) + shift;
+          }
+          
+        }
+      }
+      return result;
+    }catch (error){
+      return console.log(error);
+    }
   }
 
   return {
--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,8 @@
 // Write your tests here!
+//test for greater than 25 should return false
+//test for less than -25 should retun false
+//test for 0 should return false
+
+//test for positive shift value
+//test for negative shift value
+//test for false flag to decode
