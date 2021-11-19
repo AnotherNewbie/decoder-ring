93ac6dfec6c16d608e4b4d82b6c1a1d4c07c969d
Michael Holthausen <mgholthausen@gmail.com>
2021-11-19 16:09:50 -0500

    added tests

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,5 +1,27 @@
+const { expect } = require("chai");
+const { caesar } = require("../src/caesar");
+describe("caesar() submission tests written by Me", () => {
 // Write your tests here!
 // test for greater than 25 should return false
+    describe("error handling", () => {
+        it("should return false if shift is greater than 25",() =>{
+            const message = "Hello World!";
+            const shift = 26;
+            const actual = caesar(message, shift);
+            expect(actual).to.be.false;
+        });
+        it("should return false if shift is less than -25",() =>{
+            const message = "Hello World!";
+            const shift = -26;
+            const actual = caesar(message, shift);
+            expect(actual).to.be.false;
+        });
+    });
+    describe("encoding tests", () =>{
+        
+    });
+});
+
 // test for less than -25 should retun false
 // test for 0 should return false
 
