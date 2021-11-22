aa61fe362837914515da41169808bf35d349fb63
Michael Holthausen <mgholthausen@gmail.com>
2021-11-22 10:14:59 -0500

    fixed non alphabet characters

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,25 +1,38 @@
 const { expect } = require("chai");
 const { caesar } = require("../src/caesar");
 describe("caesar() submission tests written by Me", () => {
-// Write your tests here!
-// test for greater than 25 should return false
-    describe("error handling", () => {
-        it("should return false if shift is greater than 25",() =>{
-            const message = "Hello World!";
-            const shift = 26;
-            const actual = caesar(message, shift);
-            expect(actual).to.be.false;
-        });
-        it("should return false if shift is less than -25",() =>{
-            const message = "Hello World!";
-            const shift = -26;
-            const actual = caesar(message, shift);
-            expect(actual).to.be.false;
-        });
+  // Write your tests here!
+  // test for greater than 25 should return false
+  describe("error handling", () => {
+    it("should return false if shift is greater than 25", () => {
+      const message = "Hello World!";
+      const shift = 26;
+      const actual = caesar(message, shift);
+      expect(actual).to.be.false;
     });
-    describe("encoding tests", () =>{
-        
+    it("should return false if shift is less than -25", () => {
+      const message = "Hello World!";
+      const shift = -26;
+      const actual = caesar(message, shift);
+      expect(actual).to.be.false;
+    });    
+  });
+  describe("encoding tests", () => {
+      it("should maintain characters other than alphanumerics", () => {
+        const message  = "123 !"
+        const shift = 1;
+        const actual = caesar(message, shift);
+        expect(actual).to.eql(message);
+      });
+  });
+  describe("decoding tests", () => {
+    it("should maintain characters other than alphanumerics", () => {
+        const message  = "123 !"
+        const shift = 1;
+        const actual = caesar(message, shift, false);
+        expect(actual).to.eql(message);
     });
+  });
 });
 
 // test for less than -25 should retun false
