a4f0786c8ce296f52216a4ef21e45f322a9deb05
Michael Holthausen <mgholthausen@gmail.com>
2021-11-22 11:22:58 -0500

    prettified substitution

--- src/substitution.js
+++ src/substitution.js
@@ -5,115 +5,115 @@
 
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
-  function encoder(input, alphabet) {    
+  function encoder(input, alphabet) {
     let subObj = {
-      "a": alphabet.charAt(0),
-      "b": alphabet.charAt(1),
-      "c": alphabet.charAt(2),
-      "d": alphabet.charAt(3),
-      "e": alphabet.charAt(4),
-      "f": alphabet.charAt(5),
-      "g": alphabet.charAt(6),
-      "h": alphabet.charAt(7),
-      "i": alphabet.charAt(8),
-      "j": alphabet.charAt(9),
-      "k": alphabet.charAt(10),
-      "l": alphabet.charAt(11),
-      "m": alphabet.charAt(12),
-      "n": alphabet.charAt(13),
-      "o": alphabet.charAt(14),
-      "p": alphabet.charAt(15),
-      "q": alphabet.charAt(16),
-      "r": alphabet.charAt(17),
-      "s": alphabet.charAt(18),
-      "t": alphabet.charAt(19),
-      "u": alphabet.charAt(20),
-      "v": alphabet.charAt(21),
-      "w": alphabet.charAt(22),
-      "x": alphabet.charAt(23),
-      "y": alphabet.charAt(24),
-      "z": alphabet.charAt(25),
+      a: alphabet.charAt(0),
+      b: alphabet.charAt(1),
+      c: alphabet.charAt(2),
+      d: alphabet.charAt(3),
+      e: alphabet.charAt(4),
+      f: alphabet.charAt(5),
+      g: alphabet.charAt(6),
+      h: alphabet.charAt(7),
+      i: alphabet.charAt(8),
+      j: alphabet.charAt(9),
+      k: alphabet.charAt(10),
+      l: alphabet.charAt(11),
+      m: alphabet.charAt(12),
+      n: alphabet.charAt(13),
+      o: alphabet.charAt(14),
+      p: alphabet.charAt(15),
+      q: alphabet.charAt(16),
+      r: alphabet.charAt(17),
+      s: alphabet.charAt(18),
+      t: alphabet.charAt(19),
+      u: alphabet.charAt(20),
+      v: alphabet.charAt(21),
+      w: alphabet.charAt(22),
+      x: alphabet.charAt(23),
+      y: alphabet.charAt(24),
+      z: alphabet.charAt(25),
     };
     let encodedMsg = "";
-    for(let i = 0; i < input.length; i++){
-      if(input.charAt(i) === " "){
+    for (let i = 0; i < input.length; i++) {
+      if (input.charAt(i) === " ") {
         encodedMsg += " ";
-      }else {
+      } else {
         encodedMsg += subObj[input.charAt(i)];
       }
     }
     return encodedMsg;
   }
 
-  function decoder (input, alphabet) {    
+  function decoder(input, alphabet) {
     let subObj = {
-      "a": alphabet.charAt(0),
-      "b": alphabet.charAt(1),
-      "c": alphabet.charAt(2),
-      "d": alphabet.charAt(3),
-      "e": alphabet.charAt(4),
-      "f": alphabet.charAt(5),
-      "g": alphabet.charAt(6),
-      "h": alphabet.charAt(7),
-      "i": alphabet.charAt(8),
-      "j": alphabet.charAt(9),
-      "k": alphabet.charAt(10),
-      "l": alphabet.charAt(11),
-      "m": alphabet.charAt(12),
-      "n": alphabet.charAt(13),
-      "o": alphabet.charAt(14),
-      "p": alphabet.charAt(15),
-      "q": alphabet.charAt(16),
-      "r": alphabet.charAt(17),
-      "s": alphabet.charAt(18),
-      "t": alphabet.charAt(19),
-      "u": alphabet.charAt(20),
-      "v": alphabet.charAt(21),
-      "w": alphabet.charAt(22),
-      "x": alphabet.charAt(23),
-      "y": alphabet.charAt(24),
-      "z": alphabet.charAt(25)
+      a: alphabet.charAt(0),
+      b: alphabet.charAt(1),
+      c: alphabet.charAt(2),
+      d: alphabet.charAt(3),
+      e: alphabet.charAt(4),
+      f: alphabet.charAt(5),
+      g: alphabet.charAt(6),
+      h: alphabet.charAt(7),
+      i: alphabet.charAt(8),
+      j: alphabet.charAt(9),
+      k: alphabet.charAt(10),
+      l: alphabet.charAt(11),
+      m: alphabet.charAt(12),
+      n: alphabet.charAt(13),
+      o: alphabet.charAt(14),
+      p: alphabet.charAt(15),
+      q: alphabet.charAt(16),
+      r: alphabet.charAt(17),
+      s: alphabet.charAt(18),
+      t: alphabet.charAt(19),
+      u: alphabet.charAt(20),
+      v: alphabet.charAt(21),
+      w: alphabet.charAt(22),
+      x: alphabet.charAt(23),
+      y: alphabet.charAt(24),
+      z: alphabet.charAt(25),
     };
     let dencodedMsg = "";
 
-    for(let i = 0; i < input.length; i++){
-      if(input.charAt(i) === " "){
+    for (let i = 0; i < input.length; i++) {
+      if (input.charAt(i) === " ") {
         dencodedMsg += " ";
-      }else {
-        dencodedMsg += Object.keys(subObj).find(key => subObj[key] === input.charAt(i));
+      } else {
+        dencodedMsg += Object.keys(subObj).find(
+          (key) => subObj[key] === input.charAt(i)
+        );
       }
     }
     return dencodedMsg;
   }
 
   function substitution(input, alphabet, encode = true) {
-    
     // your solution code here
-    if (alphabet === undefined){
+    if (alphabet === undefined) {
       return false;
     }
-    if(alphabet.length !== 26){
+    if (alphabet.length !== 26) {
       return false;
     }
     input.toLowerCase();
     alphabet.toLowerCase();
-    for(let i = 0; i < 26; i++){
+    for (let i = 0; i < 26; i++) {
       let max = 0;
-      for(let j = 0; j < 26; j++){
-        if(alphabet.charAt(j)===String.fromCharCode(97+i)){
+      for (let j = 0; j < 26; j++) {
+        if (alphabet.charAt(j) === String.fromCharCode(97 + i)) {
           max++;
         }
       }
-      if (max > 1){
+      if (max > 1) {
         return false;
       }
     }
-    if (encode){
+    if (encode) {
       return encoder(input, alphabet);
-    }else if (!encode){
+    } else if (!encode) {
       return decoder(input, alphabet);
     }
-
   }
 
   return {
