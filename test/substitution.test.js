e2e665c15507a5bba389c0dab3fdc762fecf8746
Michael Holthausen <mgholthausen@gmail.com>
2021-11-22 13:42:57 -0500

    added test cases

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,2 +1,31 @@
+const { expect } = require("chai");
+const { substitution } = require("../src/substitution");
 // Write your tests here!
-//check if alphanumerics other than letters work
+describe("substitution tests written by me", ()=>{
+    
+    describe("error handling", ()=>{
+        
+    });
+
+    describe("encoding",() => {
+        //check if alphanumerics other than letters work in alphabet
+        it("should allow characters other than letters in alphabet to encode",()=>{
+            const message = "i think";
+            const alphabet = "xoyqmcgrukswaflnthd1pzibev"
+            const expected = "u 1rufs"
+            const actual = substitution(message, alphabet);
+            expect(actual).to.equal(expected);
+        });
+    });
+
+    describe("decoding", () => {
+        //check if alphanumerics other than letters work in alphabet
+        it("should allow characters other than letters in alphabet to decode", () => {
+            const message = "u 1rufs";
+            const alphabet = "xoyqmcgrukswaflnthd1pzibev"
+            const expected = "i think";
+            const actual = substitution(message, alphabet, false);
+            expect(actual).to.equal(expected);
+        });
+    });
+});
