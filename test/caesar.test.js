bd5d63a2b00388dfbb6570a4e21cb05eb2fe9abc
Michael Holthausen <mgholthausen@gmail.com>
2021-11-22 12:27:43 -0500

    cleaned up test

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -34,10 +34,3 @@ describe("caesar() submission tests written by Me", () => {
     });
   });
 });
-
-// test for less than -25 should retun false
-// test for 0 should return false
-
-// test for positive shift value
-// test for negative shift value
-// test for false flag to decode
