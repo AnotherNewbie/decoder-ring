ec051df59817cf58feac7efe5b9973f4c322ac10
Michael Holthausen <mgholthausen@gmail.com>
2021-11-22 10:54:16 -0500

    first encoder pass

--- src/substitution.js
+++ src/substitution.js
@@ -5,9 +5,83 @@
 
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
+  function encoder(input, alphabet) {    
+    let subObj = {
+      "a": alphabet.charAt(0),
+      "b": alphabet.charAt(1),
+      "c": alphabet.charAt(2),
+      "d": alphabet.charAt(3),
+      "e": alphabet.charAt(4),
+      "f": alphabet.charAt(5),
+      "g": alphabet.charAt(6),
+      "h": alphabet.charAt(7),
+      "i": alphabet.charAt(8),
+      "j": alphabet.charAt(9),
+      "k": alphabet.charAt(10),
+      "l": alphabet.charAt(11),
+      "m": alphabet.charAt(12),
+      "n": alphabet.charAt(13),
+      "o": alphabet.charAt(14),
+      "p": alphabet.charAt(15),
+      "q": alphabet.charAt(16),
+      "r": alphabet.charAt(17),
+      "s": alphabet.charAt(18),
+      "t": alphabet.charAt(19),
+      "u": alphabet.charAt(20),
+      "v": alphabet.charAt(21),
+      "w": alphabet.charAt(22),
+      "x": alphabet.charAt(23),
+      "y": alphabet.charAt(24),
+      "z": alphabet.charAt(25),
+    };
+    let encodedMsg = "";
+    for(let i = 0; i < input.length; i++){
+      encodedMsg += subObj[input.charAt(i)];
+    }
+    return encodedMsg;
+  }
+
+  function decoder (input, alphabet) {
+    let subObj = {
+      "a": alphabet.charAt(0),
+      "b": alphabet.charAt(1),
+      "c": alphabet.charAt(2),
+      "d": alphabet.charAt(3),
+      "e": alphabet.charAt(4),
+      "f": alphabet.charAt(5),
+      "g": alphabet.charAt(6),
+      "h": alphabet.charAt(7),
+      "i": alphabet.charAt(8),
+      "j": alphabet.charAt(9),
+      "k": alphabet.charAt(10),
+      "l": alphabet.charAt(11),
+      "m": alphabet.charAt(12),
+      "n": alphabet.charAt(13),
+      "o": alphabet.charAt(14),
+      "p": alphabet.charAt(15),
+      "q": alphabet.charAt(16),
+      "r": alphabet.charAt(17),
+      "s": alphabet.charAt(18),
+      "t": alphabet.charAt(19),
+      "u": alphabet.charAt(20),
+      "v": alphabet.charAt(21),
+      "w": alphabet.charAt(22),
+      "x": alphabet.charAt(23),
+      "y": alphabet.charAt(24),
+      "z": alphabet.charAt(25)
+    };
+    return;
+  }
 
   function substitution(input, alphabet, encode = true) {
     // your solution code here
+
+    if (encode){
+      return encoder(input, alphabet);
+    }else if (!encode){
+      return decoder(input, alphabet);
+    }
+
   }
 
   return {
