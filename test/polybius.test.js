67d9bc1fefd343f2dcc7796bce8c1714587da134
Michael Holthausen <mgholthausen@gmail.com>
2021-11-22 11:51:35 -0500

    added tests

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,5 +1,24 @@
+const { expect } = require("chai");
+const { polybius } = require("../src/polybius");
 // Write your tests here!
-// number of characters in the string should be even, excluding spaces
-// only spaces and characters
-// spaces must be maintained througout
-// letter i&j are both 42 but when decoding should show i/j for 42
+describe("polybius() tests written by Me", () => {
+    describe("error handling", ()=>{
+    // numer of characters in the string should be even, excluding spaces when decoding
+        it("should return false if the string has odd characters, excluding spaces",() => {
+            const message = "111 "
+            const actual = polybius(message, false);
+            expect(actual).to.be.false;
+        });
+    });
+    // only spaces and characters
+    describe("encoding",() => {
+    // spaces must be maintained througout
+    });
+    describe("decoding", () => {
+    // spaces must be maintained througout
+    // letter i&j are both 42 but when decoding should show i/j for 42
+    });
+    
+
+
+});
